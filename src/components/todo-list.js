b7b5e2ddd0ac1b95da1cf8d7f0564247cf92ef47
Mykola Syvak <mykola.syvak@gmail.com>
2020-04-01 17:11:32 +0300

    30 finish

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import TodoLstItem from './todo-list-item';
-const TodoList = () => {
+import TodoListItem from './todo-list-item';
+
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+        return (
+          <li><TodoListItem { ...item } />
+          </li>
+        );
+    });
+
     return (
       <ul>
-          <li><TodoLstItem label="Drink Coffee"/></li>
-          <li><TodoLstItem
-           label="Build React App"
-           important /></li>
+          { elements }
       </ul>
     );
   };
