94483feff94e49b0d430ad1947911cb11ff36eb7
Mykola Syvak <mykola.syvak@gmail.com>
2020-04-07 20:06:10 +0300

    41 in progress

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,7 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
@@ -15,8 +16,8 @@ export default class App extends Component {
     todoData: [
       { label: 'Drink Coffee', important: false, id: 1 },
       { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 }
-    ]
+      { label: 'Have a lunch', important: false, id: 3 },
+    ],
   };
 
   deleteItem = (id) => {
@@ -46,6 +47,7 @@ export default class App extends Component {
         <TodoList
           todos={this.state.todoData}
           onDeleted={ this.deleteItem } />
+        <ItemAddForm />
       </div>
     );
   }
---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css';
+
+export default class ItemAddFrom extends Component {
+    render() {
+        return (
+            <div className="item-add-form">
+                <button className="btn btn-outline-secondary"
+                        onClick={console.log("ADD")}>
+                        Add Item
+                </button>
+            </div>
+        )
+    }
+}
