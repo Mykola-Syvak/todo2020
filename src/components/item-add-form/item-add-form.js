a92832d4bcf3536ad28e4ec9b3d4170dac3eb911
Mykola Syvak <mykola.syvak@gmail.com>
2020-04-07 20:47:34 +0300

    41 in progress

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddFrom extends Component {
+    
+   onButtonAddClick = () => console.log('URA');
+
     render() {
         return (
             <div className="item-add-form">
                 <button className="btn btn-outline-secondary"
-                        onClick={console.log("ADD")}>
+                        onClick={this.onButtonAddClick}>
                         Add Item
                 </button>
             </div>
